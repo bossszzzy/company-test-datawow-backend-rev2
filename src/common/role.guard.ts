c6d5fd3c7f23c190ae6bd87f504b3469594b2e1c
bossszzzy <bossasinate@gmail.com>
2025-10-26 20:24:05 +0700

    role guard

---
+++ src/common/role.guard.ts
@@ -0,0 +1,22 @@
+import { CanActivate, ExecutionContext, ForbiddenException, Injectable } from '@nestjs/common';
+import { Role } from 'generated/prisma/enums';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class RoleGuard implements CanActivate {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const req = context.switchToHttp().getRequest()
+    const role = req.headers['x-role']
+    if (role !== 'ADMIN') throw new ForbiddenException('Admin Only')
+    return true;
+  }
+
+}
+
+export function getUserId(req: any): string {
+  const id = req.headers['x-user-id'];
+  if (!id || typeof id !== 'string') throw new ForbiddenException('x-user-id required');
+  return id
+}
--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
 
   app.useGlobalPipes(new GlobalValidationPipe())
   app.useGlobalFilters(new BaseHttpFilter())
-
+  app.setGlobalPrefix('api')
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap().catch((error) => {
